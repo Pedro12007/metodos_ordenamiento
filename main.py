94b8a284f4c2913535d043367c682740b8e17209
Karup <kperca802@gmail.com>
2025-10-16 09:23:14 -0600

    Bogo Sort

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 n = int(input('¿Cuántos números desea ingresar?: '))
 
 lista = []
@@ -6,13 +7,24 @@ for i in range(n):
     num = int(input(f'{i+1}. Ingrese un número: '))
     lista.append(num)
 
-def selection_sort():
+def selection_sort(lista):
     for i in range (len(lista)):
         min_index = i
         for j in range(i+1, len(lista)):
             if lista[j]<lista[min_index]:
                 min_index = j
         lista[i], lista[min_index] = lista[min_index], lista[i]
+
+def esta_ordenada(lista):
+    return all(lista[i]<= lista[i+1] for i in range(len(lista)-1))
+
+def bogo_sort(lista):
+    intentos = 0
+    while not esta_ordenada(lista):
+        random.shuffle(lista)
+        intentos += 1
+    print(f"Ordenada después de {intentos} intentos")
+    return lista
 
 while True:
     print('Opciones: ')
