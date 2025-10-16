a0afc150d709a51cc148f43599b8aa93036fe7a3
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-10-16 09:07:57 -0600

    Ingreso de números a lista

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
+n = int(input('¿Cuántos números desea ingresar?: '))
+
+lista = []
+
+for i in range(n):
+    num = int(input(f'{i+1}. Ingrese un número: '))
+    lista.append(num)
+
+
 while True:
+    print('Opciones: ')
+    print('1. Bubble Sort')
+    print('2. Selection Sort')
+    print('3. Quick Sort')
+    print('4. Bogo Sort')
+    print('5. Desordenar lista')
+    print('6. Salir')
+
     option = input('Ingrese la opción deseada: ')
 
     match option:
+        case '1':
+            pass
+
+        case '2':
+            pass
+
+        case '3':
+            pass
+
+        case '4':
+            pass
 
         case '5':
+            pass
+
+        case '6':
+            print('Saliendo del programa...')
             break
+
+        case _:
+            print('Opción incorrecta. Intente nuevamente.\n')
