7ae9cfb4a2123930f16e97b1b9f8a3b219b4054c
Karup <kperca802@gmail.com>
2025-10-16 09:15:40 -0600

    Selection Sort

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ for i in range(n):
     num = int(input(f'{i+1}. Ingrese un número: '))
     lista.append(num)
 
+def selection_sort():
+    for i in range (len(lista)):
+        min_index = i
+        for j in range(i+1, len(lista)):
+            if lista[j]<lista[min_index]:
+                min_index = j
+        lista[i], lista[min_index] = lista[min_index], lista[i]
 
 while True:
     print('Opciones: ')
